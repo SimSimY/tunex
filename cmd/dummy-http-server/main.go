13e40c3f0d4fd11659d14ec8e8ddd236360c2b64
Simon Yakov <simon@s-m-y.com>
2024-04-27 22:09:20 +0300

    dummy server

--- cmd/dummy-http-server/main.go
+++ cmd/dummy-http-server/main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/SimSimY/tunex/config"
+	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"strconv"
+	"strings"
 )
 
 type DummyServer struct{}
 
+type HttpDumpResponse struct {
+	Headers http.Header `json:"headers"`
+	Body    string      `json:"body"`
+	Method  string      `json:"method"`
+	URL     string      `json:"url"`
+	Form    url.Values  `json:"form"`
+}
+
 func (h *DummyServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	buf := new(strings.Builder)
 	w.WriteHeader(http.StatusOK)
+	_, _ = io.Copy(buf, r.Body)
+	dumpResponse := &HttpDumpResponse{
+		Headers: r.Header,
+		Body:    buf.String(),
+		Method:  r.Method,
+		URL:     r.URL.String(),
+		Form:    r.Form,
+	}
+	b, _ := json.Marshal(dumpResponse)
+
+	_, _ = w.Write(b)
 }
 
 func main() {
